d6893b41ae159b47f8b78aa3d1914e116a084626
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2022-10-15 16:16:11 +0300

    feature: routing in the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-
-
+import { Route, Routes } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import About from "./About"
 function App() {
   return (
     <div className="App">
-      <h1>Hello Catty</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
