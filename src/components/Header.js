5297b17ed0f618b74b9f50212d130c2b0146d11b
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2022-10-15 17:09:47 +0300

    styling:changed font of header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom"
 function Header(){
     return(
         <nav className="hover:text-gray-700 relative px-8 py-4 shadow-lg w-full items-center justify-between flex flex-wrap">
-            <h1 className="text-4xl text-yellow-400 text-left">Catty</h1>
-            <div className="space-x-4 text-2xl text-yellow-400">
+            <h1 className="text-4xl font-montserrat font-bold text-yellow-400 text-left">Catty</h1>
+            <div className="space-x-4 text-2xl font-montserrat text-yellow-400">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             {/* <NavLink to="/addacat">Add A Cat</NavLink> */}
