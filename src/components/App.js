6b3dd728ba89b68d53bc3ec08d8272e4b18b5832
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2022-12-21 19:10:39 +0300

    fix: added Home component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home";
 import About from "./About"
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Home />} />
         <Route path="/about" element={<About/>} />
+        
       </Routes>
       <Footer />
     </div>
